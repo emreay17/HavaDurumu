a38e78aae7ed43b3b30e3e7a7aeeb9725e10e1b8
Emre AY <ay.vet.hek@gmail.com>
2023-06-09 12:26:41 +0300

    açıklama satırları eklendi

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const url = 'https://api.openweathermap.org/data/2.5'
-const key = 'f50b70fd9480c0d62f6879cb84ee2ebe'
 
-
-const setQuery = (e) => {
+//  ekrandaki input a şehir ismini girip enter a bastığımızda value yu yakalıyoruz 
+ const setQuery = (e) => {
     if (e.keyCode == '13') {
+        // getResult fonksiyonu ile aldığımız value yu apiden aldığımız verilerde filtreliyoruz ve burda value yu direk fonksiyona parametre olarak atıyoruz
         getResult(searchBar.value)
     }
 }
+// 
 
+// Openweathermapten veriyi alacak bir fonksiyon yazıyoruz. API ve veriyi isteyeceğimiz linki Openweathermap den alıyoruz. 
 const getResult = (cityName) => {
-    let query =`${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+    const API = 'f50b70fd9480c0d62f6879cb84ee2ebe'
+    let query =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`;
+    
+    // url isteğimizi gerçekleştirip dönen veriyi listemizi oluşturmak için fetch kullanıp zincir yapısıyla gelen cevaplarımızı alacagımız bir fonksiyon yapıp cevapları return ediyoruz 
     fetch(query)
     .then(weather =>{
         return weather.json()
