e67fcc06241c08ae7ff1f0867a60142a7fd203ec
Emre AY <ay.vet.hek@gmail.com>
2023-06-09 15:00:41 +0300

    api added just extra css will add

--- app.js
+++ app.js
@@ -11,17 +11,22 @@
 // Openweathermapten veriyi alacak bir fonksiyon yazıyoruz. API ve veriyi isteyeceğimiz linki Openweathermap den alıyoruz. 
 const getResult = (cityName) => {
     const API = 'f50b70fd9480c0d62f6879cb84ee2ebe'
-    let query =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`;
+    let query =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}&units=metric&lang=tr`;
     
     // url isteğimizi gerçekleştirip dönen veriyi listemizi oluşturmak için fetch kullanıp zincir yapısıyla gelen cevaplarımızı alacagımız bir fonksiyon yapıp cevapları return ediyoruz 
     fetch(query)
+    //promise yapısı ile çalıştığı için json formatında return etmemiz gerekli 
     .then(weather =>{
         return weather.json()
     })
+    // zincir yapısıyla kullanmaya başlıyoruz
     .then(displayResult)
 }
-
+// artık verilerimizi ekranda göstermek için fonksiyonumuzu düzenleyebiliriz ...
 const displayResult =(result) =>{
+   
+  console.log(result);
+   
     let city = document.querySelector('.city');
     city.innerText = `${result.name}, ${result.sys.country}`;
 
@@ -39,3 +44,6 @@ const displayResult =(result) =>{
 
 const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keypress', setQuery)
+
+
+// BAzı hava durumlarına istinaden görseller ekleyebiliriz.
